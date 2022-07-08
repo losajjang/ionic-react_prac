dc4e657d124d9c0e3f14fec61e9cb11c7991aa59
losajjang <losajjang@naver.com>
2022-07-08 18:35:41 +0900

    inputUI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,10 +14,12 @@ import {
   IonItemOption,
   IonButton,
   IonIcon,
+  IonInput,
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Home.css";
 import { atCircle, fingerPrint, star } from "ionicons/icons";
+import { KeyboardEvent, useEffect, useState } from "react";
 
 const arr = [
   { name: "Finn", desc: "this guy rocks!" },
@@ -28,6 +30,14 @@ const arr = [
 ];
 
 const Home: React.FC = () => {
+  const [input, setInput] = useState<string>("");
+  const [number, setNumber] = useState<number>();
+  const [text, setText] = useState<string>("");
+
+  useEffect(() => {
+    console.log(input);
+  }, [input]);
+
   return (
     <IonPage>
       <IonHeader>
@@ -36,94 +46,39 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        <IonText>
-          <h3>1. expand 속성</h3>
-        </IonText>
-        <IonButton color="primary">expand="default"</IonButton>
-        <IonButton expand="full" color="primary">
-          expand="full"
-        </IonButton>
-        <IonButton expand="block" color="primary">
-          expand="block"
-        </IonButton>
-
-        <IonText>
-          <h3>2. fill 속성</h3>
-        </IonText>
-        <IonButton color="primary">fill="default"</IonButton>
-        <IonButton fill="clear" color="primary">
-          fill="clear"
-        </IonButton>
-        <IonButton fill="outline" color="primary">
-          fill="outline"
-        </IonButton>
-        <IonButton fill="solid" color="primary">
-          fill="solid"
-        </IonButton>
-
-        <IonText>
-          <h3>3. size 속성</h3>
-        </IonText>
-        <IonButton color="primary" size="small">
-          size="small"
-        </IonButton>
-        <IonButton color="primary" size="default">
-          size="default"
-        </IonButton>
-        <IonButton color="primary" size="large">
-          size="large"
-        </IonButton>
-
-        <IonText>
-          <h3>4. disabled 속성</h3>
-        </IonText>
-        <IonButton color="primary" disabled>
-          disabled
-        </IonButton>
-
-        <IonText>
-          <h3>5. mode 속성</h3>
-        </IonText>
-        <IonButton color="primary" mode="ios">
-          mode="ios"
-        </IonButton>
-        <IonButton color="primary" mode="md">
-          mode="md"
-        </IonButton>
-
-        <IonText>
-          <h3>6. shape 속성</h3>
-        </IonText>
-        <IonButton color="primary" shape="round">
-          shape="round"
-        </IonButton>
-
-        <IonText>
-          <h3>7. strong 속성</h3>
-        </IonText>
-        <IonButton color="primary" strong>
-          strong
-        </IonButton>
-
-        <IonText>
-          <h3>8. slot 속성</h3>
-        </IonText>
-        <IonButton color="primary">
-          <IonIcon slot="" icon={star}></IonIcon>
-          slot=""
-        </IonButton>
-        <IonButton color="primary">
-          <IonIcon slot="end" icon={star}></IonIcon>
-          slot="end"
-        </IonButton>
-        <IonButton color="primary">
-          <IonIcon slot="icon-only" icon={star}></IonIcon>
-          slot="icon-only"
-        </IonButton>
-        <IonButton color="primary">
-          <IonIcon slot="start" icon={star}></IonIcon>
-          slot="start"
-        </IonButton>
+        <IonItem>
+          <IonInput
+            placeholder="with placeholder, without label"
+            onIonChange={(e) => setText(e.detail.value!)}
+          ></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonInput
+            type="number"
+            value={number}
+            placeholder="Enter Number"
+            onIonChange={(e) => setNumber(parseInt(e.detail.value!, 10))}
+          ></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonInput placeholder="disabled input" value={text} disabled></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">clear Input</IonLabel>
+          <IonInput onIonChange={(e: any) => setInput(e.target.value)} clearInput />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">stacked label</IonLabel>
+          <IonInput onIonChange={(e: any) => setInput(e.target.value)} />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">Floating Label</IonLabel>
+          <IonInput value={text}></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="fixed">Fixed Label</IonLabel>
+          <IonInput value={text}></IonInput>
+        </IonItem>
       </IonContent>
     </IonPage>
   );
