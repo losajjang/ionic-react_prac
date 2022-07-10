9d7910f21d49581724d8145fc596dab3a42f0864
losajjang <losajjang@naver.com>
2022-07-11 00:32:36 +0900

    feat: toast 기능 추가 login시 상태알림을 위한 toast 기능

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -15,6 +15,7 @@ import {
   IonButton,
   IonIcon,
   IonInput,
+  useIonToast,
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Home.css";
@@ -22,14 +23,20 @@ import { atCircle, fingerPrint, star } from "ionicons/icons";
 import { KeyboardEvent, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { loginUser } from "../firebaseConfig";
+import { totalmem } from "os";
 
 const Login: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [message, dismiss] = useIonToast();
 
   async function login() {
     const res = await loginUser(username, password);
-    console.log(`${res ? "login success" : "login failed"}`);
+    if (!res) {
+      message("Error logging with your credentials", 2000);
+    } else {
+      message("You have logged in!", 2000);
+    }
   }
 
   return (
